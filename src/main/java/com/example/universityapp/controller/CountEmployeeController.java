7dcbf178fb2679ffaead40ebbfe64aa602fde53d
Laszlo Petechuk <frostpv@gmail.com>
2020-11-23 13:24:08 +0200

    added CountEmployeeController

--- src/main/java/com/example/universityapp/controller/CountEmployeeController.java
+++ src/main/java/com/example/universityapp/controller/CountEmployeeController.java
@@ -1,4 +1,31 @@
 package com.example.universityapp.controller;
 
-public class CountEmployeeController {
+import com.example.universityapp.model.Department;
+import com.example.universityapp.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class CountEmployeeController implements  CommandExecutor{
+    private static final String COUNT_OF_EMPLOYEE_COMMAND = "show count of employee for";
+    private final DepartmentService departmentService;
+
+    @Override
+    public void run(String command) {
+        String departmentName = command.replaceAll(COUNT_OF_EMPLOYEE_COMMAND, "").trim();
+        Optional<Department> departmentOptional
+                = Optional.ofNullable(departmentService.findByDepartmentNameIgnoreCase(departmentName));
+        if(departmentOptional.isPresent()) {
+            System.out.println(departmentOptional.get().getLectorList().size());
+        } else {
+            System.out.println("Invalid name of department " + departmentName);
+        }
+    }
+
+    @Override
+    public boolean isCommandValid(String command) {
+        return command.contains(COUNT_OF_EMPLOYEE_COMMAND);
+    }
 }
